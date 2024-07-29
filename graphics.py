03d43681ca6024b4f2cb20072d02b5ceee517fe3
Brian Ott <bott@b1-systems.de>
2024-07-29 12:18:45 +0200

    update: draw line method to create a line

--- graphics.py
+++ graphics.py
@@ -41,5 +41,7 @@ class Line:
         self.p2 = point2
 
 
-    def draw():
-        ...
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
