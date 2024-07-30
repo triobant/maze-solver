467d2b1d31b59b670fc8a6c3f5298b64bf64fe22
Brian Ott <bott@b1-systems.de>
2024-07-30 15:42:09 +0200

    update: window class with an additional method to draw the line in canvas

--- graphics.py
+++ graphics.py
@@ -4,10 +4,11 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -19,6 +20,10 @@ class Window:
         while self.__running:
             self.redraw()
         print("window closed...")
+
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
 
 
     def close(self):
