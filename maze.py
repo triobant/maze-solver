9654da8a48fda1ec56bf07a5479127aed1d14eb8
Brian Ott <bott@b1-systems.de>
2024-08-03 12:41:36 +0200

    update: create cell method to draw cells

--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
-from graphics import Window
 from cell import Cell
+import random
+import time
 
 
 class Maze:
@@ -13,9 +14,29 @@ class Maze:
         cell_size_y,
         win,
     ):
-       self.__x1 = x1
-       self.__y1 = y1
-       self.num_rows = num_rows
-       self.num_cols = num_cols
-       self.cell_size_x = cell_size_x
-       self.cell_size_y = cell_size_y
+       self._cells = []
+       self._x1 = x1
+       self._y1 = y1
+       self._num_rows = num_rows
+       self._num_cols = num_cols
+       self._cell_size_x = cell_size_x
+       self._cell_size_y = cell_size_y
+       self._win = win
+
+
+       self._create_cells(self)
+
+
+    def _create_cells(self):
+        for i in range(self._num_cols):
+            col_cells = []
+            for j in range(self._num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+
+
+    def _draw_cell(self, i, j):
+        ...
