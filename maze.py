59e2c47b12191f64d6e56cdb8de735f91d572530
Brian Ott <bott@b1-systems.de>
2024-08-04 20:06:52 +0200

    update: draw cell method to draw cells and the maze iteself

--- maze.py
+++ maze.py
@@ -39,4 +39,11 @@ class Maze:
 
 
     def _draw_cell(self, i, j):
-        ...
+        if self._win is None:
+            return
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
