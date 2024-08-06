b9ce69e1d4946ecf40cb85e8ec8474904ea11202
Brian Ott <bott@b1-systems.de>
2024-08-06 14:39:11 +0200

    update: second test added for maze class

--- tests.py
+++ tests.py
@@ -17,5 +17,19 @@ class Tests(unittest.TestCase):
         )
 
 
+    def test_maze_create_cells(self):
+        num_cols = 16
+        num_rows = 12
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
+
 if __name__ == '__main__':
     unittest.main()
