0da7b1ec216b5473bb89b4a0b01cb0c106e7c356
Brian Ott <bott@b1-systems.de>
2024-08-09 12:54:50 +0200

    update: int for the seed var

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     win = Window(screen_x, screen_y)
 
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
 
 
     win.wait_for_close()
